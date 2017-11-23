79fe3057407c9a9b89835c07939a16032e6e62e2
smat0096 <112093112@qq.com>
2017-11-23 17:52:35 +0800

    Daily Submission

--- src/directive/permitFunc/index.js
+++ src/directive/permitFunc/index.js
@@ -2,9 +2,8 @@ import permitFunc from './permitFunc.js'
 
 const PermitFunc = {};
 
-const install = function(Vue) {
+PermitFunc.install = function(Vue) {
   Vue.directive('permit-func', permitFunc)
 }
 
-PermitFunc.install = install
 export default PermitFunc
