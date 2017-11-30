62ab6aca1dff16f5cc2594cfcfd0362050c3957b
smat0096 <112093112@qq.com>
2017-11-30 15:55:24 +0800

    Daily Submission

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"https://api-prod"',
-    APP_ORIGIN: '"https://wallstreetcn.com"'
+    BASE_API: '"https://www.0735qxd.com"',
+    APP_ORIGIN: '"https://www.0735qxd.com"'
 };
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"sit"',
-    BASE_API: '"https://www.0735qxd.com/admin"',
+    BASE_API: '"https://www.0735qxd.com"',
     APP_ORIGIN: '"https://www.0735qxd.com"'
 };
