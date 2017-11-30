bf9bdc2e1f926eb20a7c64a37dfcc7cdcd17ff49
smat0096 <112093112@qq.com>
2017-11-30 12:00:02 +0800

    Daily Submission

--- config/sit.env.js
+++ config/sit.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"sit"',
-    BASE_API: '""',
+    BASE_API: '"https://www.0735qxd.com/admin"',
     APP_ORIGIN: '"https://www.0735qxd.com"'
 };
